80c3c1d991289b5ba551c86f75750705ec2d0806
Connor Haines <connor_haines@outlook.com>
2024-12-06 01:13:17 -0700

    small txt bubble format change

--- frontend/components/chat/message.tsx
+++ frontend/components/chat/message.tsx
@@ -8,43 +8,47 @@ export interface MessageTextProps {
 
 export function AIMessageText(props: MessageTextProps) {
   return (
-    <div className="flex mr-auto w-fit max-w-[700px] bg-gray-200 rounded-md px-2 py-1 mt-3">
+    <div className="flex mr-auto w-fit max-w-[700px] bg-gray-200 rounded-md px-2 py-1">
       <div className="text-normal text-gray-800 text-left break-words">
         <Markdown
           className="max-w-[650px]"
           remarkPlugins={[remarkGfm]}
           children={props.content}
           components={{
-            a: ({ node, ...props }) => <a {...props} />,
-            blockquote: ({ node, ...props }) => <blockquote {...props} />,
-            br: ({ node, ...props }) => <br {...props} />,
-            em: ({ node, ...props }) => <em {...props} />,
+            a: ({ node, ...props }) => <a {...props} className="my-2" />,
+            blockquote: ({ node, ...props }) => (
+              <blockquote {...props} className="my-2" />
+            ),
+            br: ({ node, ...props }) => <br {...props} className="my-2" />,
+            em: ({ node, ...props }) => <em {...props} className="my-2" />,
             h1: ({ node, ...props }) => (
-              <h1 {...props} className="text-3xl font-bold" />
+              <h1 {...props} className="my-2 text-3xl font-bold" />
             ),
             h2: ({ node, ...props }) => (
-              <h2 {...props} className="text-2xl font-bold" />
+              <h2 {...props} className="my-2 text-2xl font-bold" />
             ),
             h3: ({ node, ...props }) => (
-              <h3 {...props} className="text-xl font-bold" />
+              <h3 {...props} className="my-2 text-xl font-bold" />
             ),
             h4: ({ node, ...props }) => (
-              <h4 {...props} className="text-lg font-bold" />
+              <h4 {...props} className="my-2 text-lg font-bold" />
             ),
             h5: ({ node, ...props }) => (
-              <h5 {...props} className="text-base font-bold" />
+              <h5 {...props} className="my-2 text-base font-bold" />
             ),
             h6: ({ node, ...props }) => (
-              <h6 {...props} className="text-sm font-bold" />
+              <h6 {...props} className="my-2 text-sm font-bold" />
             ),
-            hr: ({ node, ...props }) => <hr {...props} />,
-            img: ({ node, ...props }) => <img {...props} />,
-            li: ({ node, ...props }) => <li {...props} />,
-            ol: ({ node, ...props }) => <ol {...props} />,
-            p: ({ node, ...props }) => <p {...props} />,
-            pre: ({ node, ...props }) => <pre {...props} />,
-            strong: ({ node, ...props }) => <strong {...props} />,
-            ul: ({ node, ...props }) => <ul {...props} />,
+            hr: ({ node, ...props }) => <hr {...props} className="my-2" />,
+            img: ({ node, ...props }) => <img {...props} className="my-2" />,
+            li: ({ node, ...props }) => <li {...props} className="my-2" />,
+            ol: ({ node, ...props }) => <ol {...props} className="my-2" />,
+            p: ({ node, ...props }) => <p {...props} className="my-2" />,
+            pre: ({ node, ...props }) => <pre {...props} className="my-2" />,
+            strong: ({ node, ...props }) => (
+              <strong {...props} className="my-2" />
+            ),
+            ul: ({ node, ...props }) => <ul {...props} className="my-2" />,
             code: (props) => {
               const { children, className, node, ...rest } = props;
               const match = /language-(\w+)/.exec(className || "");
@@ -66,7 +70,7 @@ export function AIMessageText(props: MessageTextProps) {
 export function HumanMessageText(props: MessageTextProps) {
   return (
     <div className="flex ml-auto w-fit max-w-[700px] bg-blue-400 rounded-md px-2 py-1">
-      <p className="text-normal text-gray-50 text-left break-words">
+      <p className="my-1 text-normal text-gray-50 text-left break-words">
         {props.content}
       </p>
     </div>
